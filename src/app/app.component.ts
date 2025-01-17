d29a30746e6f63eeeb121fe22dbce9bd80791832
Siddarthg-Dev <siddarthg.dev@gmail.com>
2025-01-17 15:40:38 +0530

    Bugs Fixed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {}
+export class AppComponent implements OnInit {
+  currecntComp: string = 'home';
+  screenWidth: number = window.innerWidth;
+  screenHeight: number = window.innerHeight;
+
+  constructor(private router: Router) {}
+
+  ngOnInit() {
+    this.router.events.subscribe((event) => {
+      if (event instanceof NavigationEnd) {
+        const urlSegment = event.urlAfterRedirects.split('/').pop() || 'home';
+        this.currecntComp = urlSegment;
+      }
+    });
+  }
+
+  // Listener for screen resize
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.screenWidth = window.innerWidth;
+    this.screenHeight = window.innerHeight;
+  }
+}
